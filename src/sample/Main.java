d13fea09e322011f50e17e604e414e7b425e6b02
carolinabado <cmbadocortes8401@eagle.fgcu.edu>
2019-10-07 19:29:00 -0400

    added updated fxml file and renamed it "bulletin"
    @ Neel & Karl

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("bulletin.fxml"));
     primaryStage.setTitle("Hello World");
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
