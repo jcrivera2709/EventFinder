5ff13a82fa12233f0247ddfbe57d4efdd8d657df
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-10-10 15:44:46 -0400

    Got some version off the map to show up on GUI.

--- src/Application/Controller.java
+++ src/Application/Controller.java
@@ -1,4 +1,22 @@
 package Application;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
 public class Controller {
+
+  @FXML
+  private WebView googleMaps = new WebView();
+
+  public void initialize() {
+    WebEngine engine = googleMaps.getEngine();
+    double longitude = 26.4556419;
+    double latitude = -81.768879;
+    double zoom = 14;
+    String zoomString = zoom +"z";
+    engine.load("https://www.google.com/maps/@" + longitude + "," + latitude + "," + zoomString);
+
+  }
+
 }
