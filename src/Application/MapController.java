17b776dc86ea917e1cffd4aeaa6abf81ae539681
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-10-15 11:23:07 -0400

    Moved webview to new scene.

--- src/Application/MapController.java
+++ src/Application/MapController.java
@@ -1,5 +1,21 @@
 package Application;
 
+import javafx.fxml.FXML;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+
 public class MapController {
 
+  @FXML
+  private WebView googleMaps = new WebView();
+
+  public void initialize() {
+    WebEngine engine = googleMaps.getEngine();
+    double longitude = 26.4556419;
+    double latitude = -81.768879;
+    double zoom = 14;
+    String zoomString = zoom + "z";
+    engine.load("https://www.google.com/maps/@" + longitude + "," + latitude + "," + zoomString);
+
+  }
 }
