dcb451b1d277be759f93dd8a52290e9315eccb4c
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-10-19 23:14:25 -0400

    Applied google styles.

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -4,10 +4,6 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 import java.lang.StringBuilder;
 
