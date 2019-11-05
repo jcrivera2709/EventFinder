b4048152dfc02e3427d8b23d84540034d30406bf
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-11-05 14:50:17 -0500

    Added functionality to username and password.  Added admin page fxml and controller.

--- src/Application/Login.java
+++ src/Application/Login.java
@@ -1,12 +1,17 @@
 package Application;
 
 import java.awt.Desktop;
+import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class Login {
 
@@ -17,15 +22,35 @@ public class Login {
   public void initialize() {
   }
 
-  public void LogInButton(ActionEvent actionEvent) {
+  public void LogInButton(ActionEvent actionEvent) throws IOException {
+
+    Stage stage = null;
+    Parent root = null;
 
     HashMap<String, String> username = new HashMap<>();
 
+    // list of user names and passwords that are acceptable in the pwField and userField
+    // using HashMap
     username.put("user", "1234");
     username.put("admin", "password");
 
     if (username.containsKey(userField.getText())) {
-
+      if (username.get(userField.getText()).equals("password")) {
+        stage = (Stage) loginButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("AdminMenu.fxml"));
+      } else if (username.get(userField.getText()).equals("1234")){
+        stage = (Stage) loginButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+      } else {
+        stage = (Stage) loginButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+      }
     }
+    // Gets root from if statement.
+    // Scene width and height are both defined in main.
+    Scene scene = new Scene(root, Main.SCENE_WIDTH, Main.SCENE_HEIGHT);
+    stage.setScene(scene);
+    scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
+    stage.show();
   }
 }
