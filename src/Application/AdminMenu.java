7a7efb607099ea7ef96035ba86392f233a369b73
Pipo <jaruizramon7195@eagle.fgcu.edu>
2019-11-07 15:09:58 -0500

    Added csv data reader, will move the reading data algo to another class since we're going to recycle the algo.

--- src/Application/AdminMenu.java
+++ src/Application/AdminMenu.java
@@ -1,5 +1,50 @@
 package Application;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.HashMap;
+
+
 public class AdminMenu {
+
+    private static HashMap<String,String> adminDataHashMap = new HashMap<>();
+
+
+    public static void setData()
+    {
+
+        try {
+
+            File data = new File("src\\Application\\data.csv");
+            Scanner fileReader = new Scanner(data);
+
+            int dataNo = 0;
+
+            while(fileReader.hasNext())
+            {
+                String line = fileReader.nextLine();
+                String[] dataLine = line.split(",");
+
+                adminDataHashMap.put(dataLine[0], dataLine[1]);
+                System.out.println(dataLine[0] + dataLine[1]);
+                dataLine = null;
+
+
+
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        for (String name : adminDataHashMap.values()) {
+            System.out.println(adminDataHashMap.values());
+        }
+
+    }
+
+
 
 }
--- src/Application/Main.java
+++ src/Application/Main.java
@@ -29,62 +29,9 @@ public class Main extends Application {
 
   public static void main(String[] args) {
 
+    AdminMenu.setData();
     launch(args);
 
-    String dataChunk = getEventData();
-    System.out.print(dataChunk);
   }
 
-  public static String getEventData() // method developed by Jose Ruiz-Ramon
-  {
-    //launch Selenium Driver
-    //declare BeautifulSoup object
-
-    String eventTitle = null;
-    String eventDate = null;
-    String eventLocation = null;
-    String eventHost = null;
-    String bigString = null;
-
-    boolean atEndOfPage = false;
-    while (atEndOfPage) {
-      // Selenium algo that will load BS4 data
-      System.out.println("click");
-      pause(2);
-
-    }
-
-    // BS algo for eventTitle
-    // BS algo for eventDate
-    // BS algo for eventLocation
-    // BS algo for eventHost
-
-    for (int i = 1; i <= LAST_ELEM; i++) {
-
-      eventTitle = "eventTitle";
-      sb.append(String.format("%s, ", eventTitle));
-
-      eventDate = "eventDate";
-      sb.append(String.format("%s, ", eventDate));
-
-      eventLocation = "eventLocation";
-      sb.append(String.format("%s, ", eventLocation));
-
-      eventHost = "eventHost";
-      sb.append(String.format("%s\n ", eventHost));
-
-    }
-
-    bigString = sb.toString();
-
-    return bigString;
-  }
-
-  public static void pause(double seconds) {
-    try {
-      Thread.sleep((long) (seconds * 1000));
-    } catch (InterruptedException e) {
-    }
-
-  }
 }
