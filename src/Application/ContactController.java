423a13218e4171cb9589a04deba4209da618cb1b
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-12-03 10:29:54 -0500

    Updated CSS file and changed small style formats.

--- src/Application/ContactController.java
+++ src/Application/ContactController.java
@@ -1,16 +1,22 @@
 package Application;
 /* Created by Neel Patel on 10/27/19. */
+
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import Application.datamodel.Contact;
 
 public class ContactController {
-  @FXML private TextField firstNameField;
-  @FXML private TextField lastNameField;
-  @FXML private TextField phoneNumberField;
-  @FXML private TextField notesField;
 
-  public Contact getNewContact() {
+  @FXML
+  private TextField firstNameField;
+  @FXML
+  private TextField lastNameField;
+  @FXML
+  private TextField phoneNumberField;
+  @FXML
+  private TextField notesField;
+
+  Contact getNewContact() {
     String firstName = firstNameField.getText();
     String lastName = lastNameField.getText();
     String phoneNumber = phoneNumberField.getText();
@@ -19,14 +25,14 @@ public class ContactController {
     return new Contact(firstName, lastName, phoneNumber, notes);
   }
 
-  public void editContact(Contact contact){
+  void editContact(Contact contact) {
     firstNameField.setText(contact.getFirstName());
     lastNameField.setText(contact.getLastName());
     phoneNumberField.setText(contact.getPhoneNumber());
     notesField.setText(contact.getNotes());
   }
 
-  public void updateContact(Contact contact){
+  void updateContact(Contact contact) {
     contact.setFirstName(firstNameField.getText());
     contact.setLastName(lastNameField.getText());
     contact.setPhoneNumber(phoneNumberField.getText());
