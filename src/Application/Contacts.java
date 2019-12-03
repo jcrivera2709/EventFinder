2c320b24f70f9463da7c2bf511e21be9ce8c8272
Jose Rivera <jcrivera2709@eagle.fgcu.edu>
2019-12-03 09:52:41 -0500

    Go the menu panel to go back to the main menu and log in page in the contacts scene.

--- src/Application/Contacts.java
+++ src/Application/Contacts.java
@@ -4,23 +4,24 @@ import Application.datamodel.Contact;
 import Application.datamodel.ContactData;
 import java.io.IOException;
 import java.util.Optional;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 /**
  * Created by Neel Patel on 10/27/19.
  */
 public class Contacts {
 
-  public MenuItem mainMenu;
-  public MenuItem logIn;
+  public MenuBar menuBar;
   @FXML
   private BorderPane mainPanel;
 
@@ -125,7 +126,39 @@ public class Contacts {
     }
   }
 
-  public void goTo(ActionEvent actionEvent) throws IOException {
+  public void goToMenu() throws IOException {
 
+    Stage stage;
+    Parent root;
+
+    // Based on button pressed if statement will load the selected scene.
+    // if not scene is selected the default scene will be the main menu.{
+    stage = (Stage) menuBar.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+
+    // Gets root from if statement.
+    // Scene width and height are both defined in main.
+    Scene scene = new Scene(root, Main.SCENE_WIDTH, Main.SCENE_HEIGHT);
+    stage.setScene(scene);
+    scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+    stage.show();
+
+  }
+
+  public void goToLogIn() throws IOException {
+    Stage stage;
+    Parent root;
+
+    // Based on button pressed if statement will load the selected scene.
+    // if not scene is selected the default scene will be the main menu.{
+    stage = (Stage) menuBar.getScene().getWindow();
+    root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+
+    // Gets root from if statement.
+    // Scene width and height are both defined in main.
+    Scene scene = new Scene(root, Main.SCENE_WIDTH, Main.SCENE_HEIGHT);
+    stage.setScene(scene);
+    scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+    stage.show();
   }
 }
